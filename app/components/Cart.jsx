e28f4b014a10b3da6ddbb0b3e26c3d07af7b70dd
Elvis Quilapi Castro <equilapi@meatcode.cl>
2024-04-10 14:21:11 -0400

    fix: layout tailwind

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,11 +1,11 @@
-import {CartForm, Image, Money} from '@shopify/hydrogen';
-import {Link} from '@remix-run/react';
-import {useVariantUrl} from '~/lib/variants';
+import { CartForm, Image, Money } from '@shopify/hydrogen';
+import { Link } from '@remix-run/react';
+import { useVariantUrl } from '~/lib/variants';
 
 /**
  * @param {CartMainProps}
  */
-export function CartMain({layout, cart}) {
+export function CartMain({ layout, cart }) {
   const linesCount = Boolean(cart?.lines?.nodes?.length || 0);
   const withDiscount =
     cart &&
@@ -23,7 +23,7 @@ export function CartMain({layout, cart}) {
 /**
  * @param {CartMainProps}
  */
-function CartDetails({layout, cart}) {
+function CartDetails({ layout, cart }) {
   const cartHasItems = !!cart && cart.totalQuantity > 0;
 
   return (
@@ -45,7 +45,7 @@ function CartDetails({layout, cart}) {
  *   lines: CartApiQueryFragment['lines'] | undefined;
  * }}
  */
-function CartLines({lines, layout}) {
+function CartLines({ lines, layout }) {
   if (!lines) return null;
 
   return (
@@ -65,9 +65,9 @@ function CartLines({lines, layout}) {
  *   line: CartLine;
  * }}
  */
-function CartLineItem({layout, line}) {
-  const {id, merchandise} = line;
-  const {product, title, image, selectedOptions} = merchandise;
+function CartLineItem({ layout, line }) {
+  const { id, merchandise } = line;
+  const { product, title, image, selectedOptions } = merchandise;
   const lineItemUrl = useVariantUrl(product.handle, selectedOptions);
 
   return (
@@ -117,13 +117,13 @@ function CartLineItem({layout, line}) {
 /**
  * @param {{checkoutUrl: string}}
  */
-function CartCheckoutActions({checkoutUrl}) {
+function CartCheckoutActions({ checkoutUrl }) {
   if (!checkoutUrl) return null;
 
   return (
-    <div>
-      <a href={checkoutUrl} target="_self">
-        <p>Continue to Checkout &rarr;</p>
+    <div className='my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 text-center focus:ring-blue-300 font-medium rounded-lg text-lg px-5 py-5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
+      <a href={checkoutUrl} target="_self" className=''>
+        Continue to Checkout &rarr;
       </a>
       <br />
     </div>
@@ -137,14 +137,14 @@ function CartCheckoutActions({checkoutUrl}) {
  *   layout: CartMainProps['layout'];
  * }}
  */
-export function CartSummary({cost, layout, children = null}) {
+export function CartSummary({ cost, layout, children = null }) {
   const className =
     layout === 'page' ? 'cart-summary-page' : 'cart-summary-aside';
 
   return (
     <div aria-labelledby="cart-summary" className={className}>
       <h4>Totals</h4>
-      <dl className="cart-subtotal">
+      <dl className="cart-subtotal flex items-center justify-between w-full my-4">
         <dt>Subtotal</dt>
         <dd>
           {cost?.subtotalAmount?.amount ? (
@@ -162,12 +162,12 @@ export function CartSummary({cost, layout, children = null}) {
 /**
  * @param {{lineIds: string[]}}
  */
-function CartLineRemoveButton({lineIds}) {
+function CartLineRemoveButton({ lineIds }) {
   return (
     <CartForm
       route="/cart"
       action={CartForm.ACTIONS.LinesRemove}
-      inputs={{lineIds}}
+      inputs={{ lineIds }}
     >
       <button type="submit">Remove</button>
     </CartForm>
@@ -177,16 +177,16 @@ function CartLineRemoveButton({lineIds}) {
 /**
  * @param {{line: CartLine}}
  */
-function CartLineQuantity({line}) {
+function CartLineQuantity({ line }) {
   if (!line || typeof line?.quantity === 'undefined') return null;
-  const {id: lineId, quantity} = line;
+  const { id: lineId, quantity } = line;
   const prevQuantity = Number(Math.max(0, quantity - 1).toFixed(0));
   const nextQuantity = Number((quantity + 1).toFixed(0));
 
   return (
     <div className="cart-line-quantity">
       <small>Quantity: {quantity} &nbsp;&nbsp;</small>
-      <CartLineUpdateButton lines={[{id: lineId, quantity: prevQuantity}]}>
+      <CartLineUpdateButton lines={[{ id: lineId, quantity: prevQuantity }]}>
         <button
           aria-label="Decrease quantity"
           disabled={quantity <= 1}
@@ -197,7 +197,7 @@ function CartLineQuantity({line}) {
         </button>
       </CartLineUpdateButton>
       &nbsp;
-      <CartLineUpdateButton lines={[{id: lineId, quantity: nextQuantity}]}>
+      <CartLineUpdateButton lines={[{ id: lineId, quantity: nextQuantity }]}>
         <button
           aria-label="Increase quantity"
           name="increase-quantity"
@@ -219,7 +219,7 @@ function CartLineQuantity({line}) {
  *   [key: string]: any;
  * }}
  */
-function CartLinePrice({line, priceType = 'regular', ...passthroughProps}) {
+function CartLinePrice({ line, priceType = 'regular', ...passthroughProps }) {
   if (!line?.cost?.amountPerQuantity || !line?.cost?.totalAmount) return null;
 
   const moneyV2 =
@@ -244,7 +244,7 @@ function CartLinePrice({line, priceType = 'regular', ...passthroughProps}) {
  *   layout?: CartMainProps['layout'];
  * }}
  */
-export function CartEmpty({hidden = false, layout = 'aside'}) {
+export function CartEmpty({ hidden = false, layout = 'aside' }) {
   return (
     <div hidden={hidden}>
       <br />
@@ -272,11 +272,11 @@ export function CartEmpty({hidden = false, layout = 'aside'}) {
  *   discountCodes: CartApiQueryFragment['discountCodes'];
  * }}
  */
-function CartDiscounts({discountCodes}) {
+function CartDiscounts({ discountCodes }) {
   const codes =
     discountCodes
       ?.filter((discount) => discount.applicable)
-      ?.map(({code}) => code) || [];
+      ?.map(({ code }) => code) || [];
 
   return (
     <div>
@@ -296,10 +296,10 @@ function CartDiscounts({discountCodes}) {
 
       {/* Show an input to apply a discount */}
       <UpdateDiscountForm discountCodes={codes}>
-        <div>
-          <input type="text" name="discountCode" placeholder="Discount code" />
+        <div className='flex items-center justify-between w-full'>
+          <input type="text" name="discountCode" placeholder="Discount code" className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
           &nbsp;
-          <button type="submit">Apply</button>
+          <button type="submit" className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Apply</button>
         </div>
       </UpdateDiscountForm>
     </div>
@@ -312,7 +312,7 @@ function CartDiscounts({discountCodes}) {
  *   children: React.ReactNode;
  * }}
  */
-function UpdateDiscountForm({discountCodes, children}) {
+function UpdateDiscountForm({ discountCodes, children }) {
   return (
     <CartForm
       route="/cart"
@@ -332,12 +332,12 @@ function UpdateDiscountForm({discountCodes, children}) {
  *   lines: CartLineUpdateInput[];
  * }}
  */
-function CartLineUpdateButton({children, lines}) {
+function CartLineUpdateButton({ children, lines }) {
   return (
     <CartForm
       route="/cart"
       action={CartForm.ACTIONS.LinesUpdate}
-      inputs={{lines}}
+      inputs={{ lines }}
     >
       {children}
     </CartForm>
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -28,43 +28,70 @@ export function Header({ header, isLoggedIn, cart }) {
     // </header>
 
 
-    <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
-          <Image
-            data={shop.brand.logo.image}
-            className="h-10"
-          />
-          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{shop.name}</span> */}
-        </a>
-        <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
-        <div id="mega-menu-full" className="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1">
-          <HeaderMenu
-            menu={menu}
-            viewport="desktop"
-            primaryDomainUrl={header.shop.primaryDomain.url}
-          />
+    // <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+    //   <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+    //     <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+    //       {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" /> */}
+    //       <Image
+    //         data={shop.brand.logo.image}
+    //         className="h-10"
+    //       />
+    //       {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{shop.name}</span> */}
+    //     </a>
+    //     <button data-collapse-toggle="mega-menu-full" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
+    //       <span className="sr-only">Open main menu</span>
+    //       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+    //         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+    //       </svg>
+    //     </button>
+    //     <div id="mega-menu-full" className="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1">
+    //       <HeaderMenu
+    //         menu={menu}
+    //         viewport="desktop"
+    //         primaryDomainUrl={header.shop.primaryDomain.url}
+    //       />
+    //     </div>
+    //   </div>
+    //   <div id="mega-menu-full-dropdown" className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600 hidden">
+    //     <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
+    //       <ul>
+    //         <li>
+    //           <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
+    //             <div className="font-semibold">Online Stores</div>
+    //             <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
+    //           </a>
+    //         </li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+
+
+    <div>
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <Image
+              data={shop.brand.logo.image}
+              className="h-10"
+            />
+            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span> */}
+          </a>
+          <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
         </div>
-      </div>
-      <div id="mega-menu-full-dropdown" className="mt-1 border-gray-200 shadow-sm bg-gray-50 md:bg-white border-y dark:bg-gray-800 dark:border-gray-600 hidden">
-        <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:px-6">
-          <ul>
-            <li>
-              <a href="#" className="block p-3 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700">
-                <div className="font-semibold">Online Stores</div>
-                <span className="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
-              </a>
-            </li>
-          </ul>
+      </nav>
+      <nav className="bg-gray-50 dark:bg-gray-700">
+        <div className="max-w-screen-xl px-4 py-3 mx-auto">
+          <div className="flex items-center">
+            <HeaderMenu
+              menu={menu}
+              viewport="desktop"
+              primaryDomainUrl={header.shop.primaryDomain.url}
+            />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
 
   );
 }
@@ -88,7 +115,7 @@ export function HeaderMenu({ menu, primaryDomainUrl, viewport }) {
   }
 
   return (
-    <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+    <ul className="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
       {viewport === 'mobile' && (
         <NavLink
           end
@@ -122,7 +149,7 @@ export function HeaderMenu({ menu, primaryDomainUrl, viewport }) {
             )
             : (
               <li>
-                <a href={url} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">{item.title}</a>
+                <a href={url} className="text-gray-900 dark:text-white hover:underline">{item.title}</a>
               </li>
             )
         );
@@ -136,9 +163,23 @@ export function HeaderMenu({ menu, primaryDomainUrl, viewport }) {
  */
 function HeaderCtas({ isLoggedIn, cart }) {
   return (
-    <nav className="header-ctas" role="navigation">
+    // <nav className="header-ctas" role="navigation">
+    //   <HeaderMenuMobileToggle />
+    //   <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
+    //     <Suspense fallback="Sign in">
+    //       <Await resolve={isLoggedIn} errorElement="Sign in">
+    //         {(isLoggedIn) => (isLoggedIn ? 'Account' : 'Sign in')}
+    //       </Await>
+    //     </Suspense>
+    //   </NavLink>
+    //   <SearchToggle />
+    //   <CartToggle cart={cart} />
+    // </nav>
+
+    <nav className="flex items-center space-x-6 rtl:space-x-reverse">
       <HeaderMenuMobileToggle />
-      <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
+      <a href="tel:5541251234" className="text-sm text-dark-500 dark:text-white hover:underline">(555) 412-1234</a>
+      <NavLink className="text-sm  text-gray-500 dark:text-white hover:underline" prefetch="intent" to="/account" style={activeLinkStyle}>
         <Suspense fallback="Sign in">
           <Await resolve={isLoggedIn} errorElement="Sign in">
             {(isLoggedIn) => (isLoggedIn ? 'Account' : 'Sign in')}
@@ -160,14 +201,14 @@ function HeaderMenuMobileToggle() {
 }
 
 function SearchToggle() {
-  return <a href="#search-aside"><FaSearch /> Search</a>;
+  return <a href="#search-aside" className="text-sm flex items-center gap-1 text-dark-500 dark:text-blue-500 hover:underline"><FaSearch /> Search</a>;
 }
 
 /**
  * @param {{count: number}}
  */
 function CartBadge({ count }) {
-  return <a href="#cart-aside"><FaShoppingCart /> Cart {count}</a>;
+  return <a className="text-sm text-dark-500 flex items-center gap-1 dark:text-blue-500 hover:underline" href="#cart-aside"><FaShoppingCart /> Cart {count}</a>;
 }
 
 /**
